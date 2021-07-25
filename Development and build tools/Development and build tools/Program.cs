40e3b0fda504c24faf2048f1a2be557307ce7d4d
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2021-07-25 20:42:19 +0300

    feat: done Development and build tools-task

--- Development and build tools/Development and build tools/Program.cs
+++ Development and build tools/Development and build tools/Program.cs
@@ -6,7 +6,24 @@ namespace Development_and_build_tools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int result = 0;
+            int maxDifferentSymbols = 1;
+            for (int i = 0; i < args[0].Length-1; i++)
+            {
+                if (args[0][i] != args[0][i + 1]) maxDifferentSymbols++;
+                if (args[0][i] == args[0][i + 1])
+                {
+                    if (result<maxDifferentSymbols) result = maxDifferentSymbols;
+                    maxDifferentSymbols = 1;
+                }
+
+            }
+
+            if (result > maxDifferentSymbols)
+            {
+                Console.WriteLine(result);
+            }
+            else Console.WriteLine(maxDifferentSymbols);
         }
     }
 }
